8661f33ae05456534e3ce14b6fcf8e737782024e
cerbereSB <marco94350@gmail.com>
2022-07-14 11:50:03 +0200

    Test de fonction test connection

--- Script de sauvegarde.ps1
+++ Script de sauvegarde.ps1
@@ -1,11 +1,12 @@
 ﻿Import-Module ActiveDirectory
 
 $Computers = Get-ADComputer -Filter 'Name -like "PARAP*"' | Select-Object -ExpandProperty Name
-
+$Date = Get-Date -Format "MM-dd-yyyy"
+$Ping = Test-Connection
 
 ForEach ($Computer in $Computers)
 {
-    Robocopy.exe "\\$Computer\c$\Users\" "E:\Sauvegardes\$Computer\$computer"  `
+       Robocopy.exe "\\$Computer\c$\Users\" "E:\Sauvegardes\$Computer\$computer"  `
     /xf "NTUSER.DAT" `
         "ntuser.dat.LOG1" `
         "ntuser.dat.LOG2" `
@@ -17,10 +18,34 @@ ForEach ($Computer in $Computers)
         'Administrateur' `
         'SRV22PARAP' `
         'SendTo' `
-        'Local Settings' /e /b /mir /mt /r:0 /w:0 /Log:E:\Log\file.log
+        'Local Settings' /e /b /mir /mt /r:0 /w:0 /log:e:\Log\$Date.txt
 }
 
 
+
+
+<#TEST DE CONNEXION
+$Ping=Test-Connection 192.168.56.24
+
+If (($ping).PingSucceeded)  { 
+
+    Write-Host "LA MACHINE EST CONNECTE" -ForegroundColor Green
+}
+
+else {
+    Write-Host "LA MACHINE EST ETEINTE" -ForegroundColor Red
+}
+#>
+
+
+
+
+
+
+
+
+
+#TESTS DE DEBUT EN DIRECT#
 <#Robocopy.exe '\\PARAPTECH01\c$\users\m.majoral' "E:\Sauvegardes" `
 /xf "NTUSER.DAT" `
     "ntuser.dat.LOG1" `
